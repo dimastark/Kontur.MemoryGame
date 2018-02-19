74a59ddfd75ea5b99c822a3bb0d823aa3e511070
Дима Старков <dstarkdev@gmail.com>
2018-02-19 22:54:56 +0500

    Not found component

--- src/components/NotFoundScreen.vue
+++ src/components/NotFoundScreen.vue
@@ -1,16 +1,24 @@
 <template>
     <layout class="container">
-        404
+        <txt>404</txt>
     </layout>
 </template>
 
 <script>
     import Layout from './Layout';
+    import Txt from './Txt';
 
     export default {
         name: 'not-found-screen',
         components: {
-            Layout
+            Layout,
+            Txt
         }
     }
 </script>
+
+<style scoped>
+    .container {
+        font-size: 100px;
+    }
+</style>
